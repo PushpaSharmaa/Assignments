4d0643dd99a5be5843ad04f6d00785a90e19e84b
PushpaSharmaa <69301406+PushpaSharmaa@users.noreply.github.com>
2021-09-24 12:48:38 +0530

    Positive_Num
    Python program to print all positive integers from a given range.

---
+++ positiveNum.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 12:19:59 2021
+
+@author: UC17
+"""
+
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [11, -21, 0, 45, 66, -93]
+
+# iterating each number in list
+for num in list1:
+	
+	# checking condition
+	if num >= 0:
+          print(num, end = " ")
+
